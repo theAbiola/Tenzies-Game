ecb718e96bd58653be0d6881c490c9f1f57c0a6a
Abdulraheem Habib Abiola <iamhabibabiola@gmail.com>
2025-05-22 09:57:03 +0100

    add dieNumbers function

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import Die from './components/Die'
 import './App.css'
 
 function App() {
-  function getRandomNumber() {
+  function getDieNumbers() {
     let numArray = []
     for (let i = 0; i < 10; i++) {
       const randomNumber = Math.ceil(Math.random() * 6)
       numArray.push(randomNumber)
     }
     return numArray
+    // Another way to write the content of this function
+    /* 
+      return new Array(10)
+         .fill(0)
+      .map(() => Math.ceil(Math.random() * 6)) 
+      */
   }
-  const dieArray = getRandomNumber()
+
+  const dieArray = getDieNumbers()
   return (
 
     <>

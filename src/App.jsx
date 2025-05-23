de5da5998bb74a7acc038cf75e1e7b7bb8341ac8
Abdulraheem Habib Abiola <iamhabibabiola@gmail.com>
2025-05-23 23:21:28 +0100

    Refactor handleDice() function to deal with the logic for tenzies

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,15 @@ import { nanoid } from "nanoid"
 function App() {
   const [dice, setDice] = useState(getDieNumbers())
 
-  function hold(id) {
-    console.log(id)
-
-    setDice(prevDice => prevDice.map(eachDie => ({
-      ...eachDie,
-      isHeld: id === eachDie.id ? !eachDie.isHeld : eachDie.isHeld
-    })))
-  }
-
   function getDieNumbers() {
     let numArray = []
     for (let i = 0; i < 10; i++) {
       const randomNumber = Math.ceil(Math.random() * 6)
-      numArray.push({ value: randomNumber, isHeld: false, id: nanoid() })
+      numArray.push({
+        value: randomNumber,
+        isHeld: false,
+        id: nanoid()
+      })
     }
     return numArray
     // Another way to write the content of this function
@@ -30,15 +25,27 @@ function App() {
       */
   }
 
+  function handleDice() {
+    setDice(prevDiceValues => prevDiceValues.map(actualDie => ({
+      ...actualDie,
+      value: actualDie.isHeld === false ? Math.ceil(Math.random() * 6) : actualDie.value
+    })))
+  }
 
+  function hold(id) {
+    console.log(id)
+
+    setDice(prevDice => prevDice.map(eachDie => ({
+      ...eachDie,
+      isHeld: id === eachDie.id ? !eachDie.isHeld : eachDie.isHeld
+    })))
+  }
 
   const dieInstances = dice.map((die, index) => (
     <Die id={die.id} key={die.id} value={die.value} isHeld={die.isHeld} holdFunction={hold} />
   ))
 
-  function handleDice() {
-    setDice(getDieNumbers())
-  }
+
 
   return (
 

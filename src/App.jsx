b3a48e8e515f6eca332e7cc7965dda4e2d568d5a
Abdulraheem Habib Abiola <iamhabibabiola@gmail.com>
2025-05-23 23:37:35 +0100

    add instructions and styling to the app

--- src/App.jsx
+++ src/App.jsx
@@ -53,6 +53,8 @@ function App() {
       <main>
         <div className='big-container'>
           <div className='small-container'>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className='dice-container'>
               {dieInstances}
             </div>
